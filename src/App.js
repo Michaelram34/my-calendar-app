58eb46154041f90339bb79b81828f932cc91f77a
Michaelram34 <michael.sd.ramirez@gmail.com>
2025-06-10 19:27:41 -0700

    Add event dialog triggered by calendar click

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ import './App.css';
 import Calendar from './Calendar';
 import EventManager from './EventManager';
 import { Container } from '@mui/material';
+import { useState } from 'react';
 
 function App() {
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateClick = (date) => {
+    setSelectedDate(date);
+    setDialogOpen(true);
+  };
+
+  const handleClose = () => {
+    setDialogOpen(false);
+  };
+
   return (
     <Container className="App">
-      <Calendar />
-      <EventManager />
+      <Calendar onDateClick={handleDateClick} />
+      <EventManager
+        open={dialogOpen}
+        onClose={handleClose}
+        defaultDate={selectedDate}
+      />
     </Container>
   );
 }
